c1767cd81a8aa94ec45749ae4ee3ea132423237e
GreatHamza124 <146306297+GreatHamza124@users.noreply.github.com>
2026-06-18 14:44:47 +1200

    Fourth Commit: Added Number Outputs and Escape sequences example

--- Output.cpp
+++ Output.cpp
@@ -2,10 +2,22 @@
 using namespace std;
 
 int main() {
+    // Text Output
     cout << "Hello World!";
+    cout << " This will be in the buffer and then printed" << endl;
+    cout << "This is a second statement \n";
+    cout << "#include <iostream> is a header library";
+    cout << "\n";
+    
+    // Number Output
+    cout << 5 << endl;
+    cout << 5+5 << endl;
 
-    cout << "This will be in the buffer and then printed" << endl;
-    cout << "This is a second statement";
+    // Escape Sequences
+    cout << "This is a new line \n";
+    cout << "This is a tab \t" << "Hello \n";
+    cout << "This is a backslash \\" << endl;
+    cout << "This is a double quote \"" << endl;
 
     return 0;
 }
