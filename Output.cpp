b71474d98688b5b69c51d46e62e27702d0002bac
GreatHamza124 <146306297+GreatHamza124@users.noreply.github.com>
2026-06-18 14:31:50 +1200

    Third Commit: New output statments added

--- Output.cpp
+++ Output.cpp
@@ -3,5 +3,9 @@ using namespace std;
 
 int main() {
     cout << "Hello World!";
+
+    cout << "This will be in the buffer and then printed" << endl;
+    cout << "This is a second statement";
+
     return 0;
 }
