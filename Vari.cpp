97680e28b7bf868aaf29745f5e799f1e992ab5c3
GreatHamza124 <146306297+GreatHamza124@users.noreply.github.com>
2026-06-18 14:51:22 +1200

    5th Commit: Added a new variables files

---
+++ Vari.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Variables
+    string name = "Me";
+    cout << "My name is " << name << "\n";
+
+    // Multiple variables
+    int a = 6;
+    int b = 7;
+    double c = 8.5;
+    cout << a + b << endl;
+    cout << b + c << endl;
+
+    return 0;
+}
